a191c47c9ea6f3cf81dc76833aa47e98b0a1431f
Margaret Choi <margaret.j.li@gmail.com>
2017-09-18 22:32:46 -0700

    Add down button

--- app/public/app.js
+++ app/public/app.js
@@ -1,10 +1,48 @@
 // Burger menu event listener
 $(document).ready(function(){
-
 	$('#menu-burger').on('click', function(e){
 		e.preventDefault();
 		$(this).toggleClass('open');
 	});
 
+
+	$('#downarrow').on("click", function( e ) {
+	    
+	    e.preventDefault();
+
+	    
+
+	    let href = $(this).attr('href');
+	    console.log(href);
+
+	    $("body, html").animate({ 
+	        scrollTop: $(href).offset().top 
+	    }, 100);
+	    
+	});
+
+
 });
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+	$('#mySidenav').css('width', '250px');
+	$('#home-wrapper').css('margin-left', '250px');
+	$('nav').css('margin-left', '290px');
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+	$('#mySidenav').css('width', '0px');
+	$('#home-wrapper').css('margin-left', '0px');
+	$('nav').css('margin-left', '40px');
+}
+
+function scrollToMiddle() {
+	$('#middle').scrollIntoView();
+	console.log('scrolling');
+}
+
+function scrollToBottom() {
+	$('#bottom').scrollIntoView();
+}
--- server.js
+++ server.js
@@ -13,13 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
 // Routing
 app.use(express.static("app/public"));
 
 app.get(
 	"/", function(req, res) {
 		res.sendFile(path.join(__dirname + '/index.html'));
+	});
+
+app.get(
+	"/portfolio", function(req, res) {
+		res.sendFile(path.join(__dirname + '/app/public/html/portfolio.html'));
+	});
+
+app.get(
+	"/contact", function(req, res) {
+		res.sendFile(path.join(__dirname + '/app/public/html/contact.html'));
 	});
 
 // app.post();
