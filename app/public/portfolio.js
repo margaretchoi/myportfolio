7135a69d48ebec0e620aba7b64541190b87f22ec
Margaret Choi <margaret.j.li@gmail.com>
2017-11-02 21:14:46 -0700

    Portfolio updates

--- app/public/portfolio.js
+++ app/public/portfolio.js
@@ -1,5 +1,27 @@
 
 $(document).ready(function(){
 
+	console.log("Running JS");
+	buildPortfolio();
 
 });
+
+let portolioImages = [
+	'/assets/images/portfolio_passport.png',
+	'/assets/images/portfolio_passport.png',
+	'/assets/images/portfolio_passport.png',
+	'/assets/images/portfolio_passport.png',
+	'/assets/images/portfolio_passport.png'
+];
+
+
+buildPortfolio = () => {
+
+	for (let i=0; i < portolioImages.length; i++) {
+		$('#portfolio-images-wrapper').append('<div class="portfolio-image"><img src="' + portolioImages[i] + '"></div>').animate({opacity: 1}, 2000, function() {
+    // Animation complete.
+  	});;
+	};
+
+}
+
